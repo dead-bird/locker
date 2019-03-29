fbd3e541ac20c1e91facfa61fca6669b6452cfd1
DaveP <dave@wearecrystal.uk>
2019-03-29 15:44:58 +0000

    Use Items

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,19 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Items/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Items from "./components/Items";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Items
   }
-}
+};
 </script>
 
 <style>
