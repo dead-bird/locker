dfe04d1bf7dae0ff53a7b5a7d986c7185237929e
DaveP <dave@wearecrystal.uk>
2019-03-29 15:43:46 +0000

    Cors

--- craft/config/element-api.php
+++ craft/config/element-api.php
@@ -9,6 +9,8 @@ use crystal\ratchet\Fields as Ratchet;
 return [
 	'endpoints' => [
 		'items' => function() {
+			Craft::$app->getResponse()->getHeaders()->set('Access-Control-Allow-Origin', '*');
+
 			return [
 				'elementType' => Entry::class,
 				'criteria' => ['section' => 'locker'],
