f26adc42a90098c3e91e00ccb4e202a85e54e467
DaveP <dave@wearecrystal.uk>
2019-03-29 15:44:45 +0000

    Items component

---
+++ app/src/components/Items.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: () => ({
+    items: []
+  }),
+
+  methods: {
+    fetch() {
+      axios
+        .get("http://localhost:8888/locker/items")
+        .then(res => {
+          this.items = res.data.data;
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  },
+
+  created() {
+    this.fetch();
+  }
+};
+</script>
+
+<template>
+  <div class="container">
+    <pre>{{ items }}</pre>
+  </div>
+</template>
+
+<style scoped>
+</style>
